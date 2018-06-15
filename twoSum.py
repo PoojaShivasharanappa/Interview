c507f455c09e0544b813c0c1e7b85f7f59fd25d2
PoojaShivasharanappa <Shivasharanappapoojaprep@gmail.com>
2018-06-14 22:35:56 -0700

    first commit

---
+++ twoSum.py
@@ -0,0 +1,23 @@
+'''
+Brute Force method
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]
+
+
+
+#Dictionary
+class Solution:
+    def twoSum(self,nums,target):
+        numDict = {}
+        for index,value in enumerate(nums):
+            numDict[value]= index
+
+        for index,value in enumerate(nums):
+            if target - value in numDict and numDict[target-value] != index:
+                return [index,numDict[target-value]]
+
+ '''
